201b6b0f74cacbe6de55bdd779e24cc6d4af4441
devsolve <naskararghya620@gmail.com>
2024-10-19 23:36:55 +0530

    variable and types: done

--- 07_variables-and-types/07_04_multiple_improve.py
+++ 07_variables-and-types/07_04_multiple_improve.py
@@ -2,3 +2,5 @@
 # Why should you change it?
 
 color, year = "red", 1900
+
+# we should give name of the variable more descriptive, which would help not only me but also other user to understand the code more effectively.
--- 07_variables-and-types/07_06_whats_my_type.py
+++ 07_variables-and-types/07_06_whats_my_type.py
@@ -2,6 +2,10 @@
 # What data types does it hold at certain points earlier in the script?
 
 mystery = None
+print(type(mystery))
 mystery = "Sommerfeld"
 mystery = 137
 mystery = mystery + 0.0
+
+# at the end of the script it's will be float. let's check
+print(type(mystery))
