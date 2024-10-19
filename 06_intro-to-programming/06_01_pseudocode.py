326beb95dbf23abff6fe1bf0e541bc5483df849d
devsolve <naskararghya620@gmail.com>
2024-10-19 23:28:06 +0530

    mecchine setup, intro programming: done, variable and type: going on

--- 06_intro-to-programming/06_01_pseudocode.py
+++ 06_intro-to-programming/06_01_pseudocode.py
@@ -6,3 +6,18 @@
 # Break this task up into a couple of steps of pseudocode
 # and write the pseudocode below in code comments.
 # You don't need to write any functional code, just map out the steps.
+
+# Step 1: Initialize a list to store all the kitten name
+kittens = []
+
+#  Step 2: Take user input of kitten name from your friend and add to the list
+kittens = list(input("enter kitten name: ").split(" "))
+
+# Step 3: Check all kitten are cute.
+for ktn in kittens:
+     is_cute = True
+     
+# Step 4: Show a message, that each kitten are cute and ready for adoption
+for kttn in kittens:
+     if is_cute:
+          print(f"{kttn} is ready for adoption")
--- 07_variables-and-types/07_01_name.py
+++ 07_variables-and-types/07_01_name.py
@@ -1,2 +1,4 @@
 # Assign your name to the variable `name`.
 # Then print it out.
+name = "Arghya Naskar"
+print(f'My name is {name}.')
